3cd1ee788e2f84d36c41f182a910c24ea9ce4122
Aleksander Alekseev <afiskon@gmail.com>
2017-07-24 15:31:24 +0300

    fix

--- src/json_example.cpp
+++ src/json_example.cpp
@@ -2,7 +2,7 @@
 
 #include "rapidjson/document.h"
 #include "rapidjson/stringbuffer.h"
-// #include "rapidjson/writer.h"
+#include "rapidjson/writer.h"
 #include <cstdint>
 #include <iostream>
 #include <sstream>
