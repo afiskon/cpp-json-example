03e3e8049a0c44092331ddb29d059474deac66fb
Aleksander Alekseev <afiskon@gmail.com>
2017-07-24 14:36:24 +0300

    fix

--- src/json_example.cpp
+++ src/json_example.cpp
@@ -1,6 +1,9 @@
 /* vim: set ai et ts=4 sw=4: */
 
 #include <iostream>
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
 
 int main() {
     std::cout << "Hello!" << std::endl;
