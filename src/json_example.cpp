dabccd33740d4c533df99c06d478da7f64b1a7b2
Aleksander Alekseev <afiskon@gmail.com>
2017-07-24 18:20:18 +0300

    Date::fromJSON() implemented

--- src/json_example.cpp
+++ src/json_example.cpp
@@ -7,6 +7,7 @@
 #include <cstdint>
 #include <iostream>
 #include <sstream>
+#include <stdexcept>
 
 class Date {
 public:
@@ -14,6 +15,28 @@ public:
       : _year(year)
       , _month(month)
       , _day(day) {
+    }
+
+    static Date fromJSON(const rapidjson::Document& doc) {
+        if(!doc.IsArray())
+            throw std::runtime_error("Date::fromJSON - document is not an array");
+
+        if(doc.Size() != 3)
+            throw std::runtime_error("Date::fromJSON - wrong array size");
+
+        uint16_t year = doc[0].GetInt();
+        uint8_t month = doc[1].GetInt();
+        uint8_t day = doc[2].GetInt();
+
+        Date result(year, month, day);
+        return result;
+    }
+
+    rapidjson::Document toJSON() {
+        rapidjson::Document doc;
+        auto& allocator = doc.GetAllocator();
+        doc.SetArray().PushBack(_year, allocator).PushBack(_month, allocator).PushBack(_day, allocator);
+        return doc;
     }
 
     uint16_t getYear() const {
@@ -37,13 +60,6 @@ public:
     Date& setDay(uint8_t day) {
         _day = day;
         return *this;
-    }
-
-    rapidjson::Document toJSON() {
-        rapidjson::Document doc;
-        auto& allocator = doc.GetAllocator();
-        doc.SetArray().PushBack(_year, allocator).PushBack(_month, allocator).PushBack(_day, allocator);
-        return doc;
     }
 
 private:
@@ -192,6 +208,9 @@ int main() {
         rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
         doc.Accept(writer);
         std::cout << buffer.GetString() << std::endl;
+
+        Date decodedDate = Date::fromJSON(doc);
+        std::cout << decodedDate << std::endl;
     }
 
     return 0;
